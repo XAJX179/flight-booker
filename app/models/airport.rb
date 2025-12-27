a269eb2ea16bdb806537c1625a4ad9291571c3f9
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-12-28 00:31:16 +0530

    fix(model): foreign key in associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,6 @@
 class Airport < ApplicationRecord
-  has_many :departing_flights, dependent: :destroy, class_name: "Flight", foreign_key: "departure_id"
-  has_many :arriving_flights, dependent: :destroy, class_name: "Flight", foreign_key: "departure_id"
+  has_many :departing_flights, dependent: :destroy, class_name: "Flight", foreign_key: "departure_airport_id"
+  has_many :arriving_flights, dependent: :destroy, class_name: "Flight", foreign_key: "departure_airport_id"
 
   validates_uniqueness_of :code
   validates_length_of :code, is: 3
