e99c0462f2379f9ca32f218592e9372e450b4b7c
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-01-05 22:18:10 +0530

    fix(spec/factories): duration type string and airports renaming

--- spec/factories/flights.rb
+++ spec/factories/flights.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :flight do
-    sequence(:duration) { |n| "#{n}:00:00" }
+    sequence(:duration) { |n| "PT2H30M#{n}S" }
     sequence(:start_time) { |n| "2025-12-#{n} 21:29:00" }
 
     departure_airport factory: :airport
