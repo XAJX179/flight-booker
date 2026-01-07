eb429348d728cd48f88d032e39c7a4bb64b7313b
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-01-07 23:38:53 +0530

    feat(app): add form for selecting flight for new booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,21 @@
+class BookingsController < ApplicationController
+  def new
+    if params_present?
+      @flight_id = bookings_params[:flight_id]
+      @no_of_passengers = bookings_params[:no_of_passengers]
+    else
+      flash[:alert] = "Please select a flight!"
+      redirect_to "/flights/index"
+    end
+  end
+
+  def params_present?
+    params.has_key?(:booking) &&
+    params.dig(:booking, :flight_id) &&
+    params.dig(:booking, :no_of_passengers)
+  end
+
+  def bookings_params
+    params.expect(booking: [ :flight_id, :no_of_passengers ])
+  end
+end
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,18 +1,26 @@
 class FlightsController < ApplicationController
   def index
     if params_present?
-      @flights = Flight.find_by_params(params)
-    else
-      @flight = Flight.new
+      @flights = Flight.find_by_params(flights_params)
+      @no_of_passengers = flights_params[:no_of_passengers]
     end
   end
 
   private
 
   def params_present?
-    params.has_key?(:start_time) &&
-    params.has_key?(:departure_airport) &&
-    params.has_key?(:arrival_airport) &&
-    params.has_key?(:no_of_passengers)
+    params.has_key?(:flight) &&
+    params.dig(:flight, :departure_airport) &&
+    params.dig(:flight, :arrival_airport) &&
+    params.dig(:flight, :start_time) &&
+    params.dig(:flight, :no_of_passengers)
+  end
+
+  def flights_params
+    params.expect(flight:
+                  [ :start_time,
+                    :departure_airport,
+                    :arrival_airport,
+                    :no_of_passengers ])
   end
 end
