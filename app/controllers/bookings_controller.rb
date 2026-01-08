df11df95c96590c45c8f04336999e9d9d88e21cd
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-01-08 17:49:24 +0530

    feat(app): rescue booking exceptions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,18 @@
 class BookingsController < ApplicationController
   def new
     if params_present?
-      flight_id = bookings_params[:flight_id]
-      @flight = Flight.find(flight_id)
       @no_of_passengers = bookings_params[:no_of_passengers]
+      unless @no_of_passengers.to_i.between?(1, 4)
+        flash[:alert] =  "Only till 4 passengers are allowed in a booking."
+        redirect_to "/flights/index"
+      end
+      begin
+        flight_id = bookings_params[:flight_id]
+        @flight = Flight.find(flight_id)
+      rescue ActiveRecord::RecordNotFound
+        flash[:alert] =  "Invalid flight??? Try again."
+        redirect_to "/flights/index"
+      end
     else
       flash[:alert] = "Please select a flight!"
       redirect_to "/flights/index"
