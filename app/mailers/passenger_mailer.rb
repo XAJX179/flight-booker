6504e1a944e88857862078192e11536138e2a21e
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-01-31 17:02:20 +0530

    rails g passenger mailer

---
+++ app/mailers/passenger_mailer.rb
@@ -0,0 +1,9 @@
+class PassengerMailer < ApplicationMailer
+  default from: "notifications@example.com"
+
+  def confirmation_mail
+    @user = params[:user]
+    @url  = "http://example.com/login"
+    mail(to: @user.email, subject: "Welcome to My Awesome Site")
+  end
+end
